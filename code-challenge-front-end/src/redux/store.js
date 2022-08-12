038eb6b665ad2c1ade6a28d3bce70a8613aa9027
basharbakkar <basharinitial.bakkar@gmail.com>
2022-08-12 16:05:15 +0300

    import reducers to store.js

--- code-challenge-front-end/src/redux/store.js
+++ code-challenge-front-end/src/redux/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-
-
+import loginReducer from "./reducers/loginReducer"
+import AlbumReducer from "./reducers/AlbumReducer";
+import commentReducer from "./reducers/commentReducer";
+import postReducer from "./reducers/PostReducer";
+import userReducer from "./reducers/usersReducer"
 
 export default configureStore({
-    
-})
-
+  reducer: {
+    posts: postReducer,
+    users: userReducer,
+    login: loginReducer,
+    album: AlbumReducer,
+    comments: commentReducer,
+  },
+});
