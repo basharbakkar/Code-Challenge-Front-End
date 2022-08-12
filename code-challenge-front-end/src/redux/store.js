95665c6b2fa94ec52715c77ec861abb2ffaacd79
basharbakkar <basharinitial.bakkar@gmail.com>
2022-08-12 20:00:10 +0300

    edit path reducer files imported in store.js

--- code-challenge-front-end/src/redux/store.js
+++ code-challenge-front-end/src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./reducers/loginReducer"
-import AlbumReducer from "./reducers/AlbumReducer";
+import AlbumReducer from "./reducers/albumReducer";
 import commentReducer from "./reducers/commentReducer";
-import postReducer from "./reducers/PostReducer";
+import postReducer from "./reducers/postReducer";
 import userReducer from "./reducers/usersReducer"
 
 export default configureStore({
